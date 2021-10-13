7ab5a5fce6c757914b4139048414b6117be9d086
eduardonk9999 <thiago.novak7@gmail.com>
2021-10-13 07:22:10 -0300

    Add, login user

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
+import { createContext, useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { firebase, auth } from './services/firebase';
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+
+export const AuthContext = createContext({} as AuthContextType);
 
 
 function App() {
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider)
+
+    if(result.user){
+      const { displayName, photoURL, uid } = result.user
+
+      if(!displayName || !photoURL) {
+        throw new Error("Nao possui todas infos da Conta Google")
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      })
+    }
+
+  }
+
   return (
-   <Home/>
+   <BrowserRouter >
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </AuthContext.Provider>
+   </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import { useContext } from 'react';
+import { AuthContext } from '../App';
+
+import { useHistory } from 'react-router-dom';
+
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logo from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -6,6 +12,19 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 export function Home() {
+  const history = useHistory();
+
+  const { user, signInWithGoogle } = useContext(AuthContext);
+
+  async function handleCreateRoom(){
+
+    if(!user){
+      await signInWithGoogle()
+    }
+
+    history.push('/rooms/new');
+  }
+
   return(
     <div id="page-auth">
       <aside>
@@ -16,7 +35,7 @@ export function Home() {
       <main>
         <div className="home-content">
           <img src={logo} alt="Logo" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
